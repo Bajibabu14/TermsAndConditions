ca4bfa70aec62f7f4b1af5ad7df12745176d29d7
Bajibabu14 <bajibabu2022@gmail.com>
2024-09-23 13:07:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import TermsAndConditions from './TermAndConditions';
+
+function App() {
+  return (
+    <div className="App">
+      <TermsAndConditions />
+    </div>
+  );
+}
+
+export default App;
+
